2ea85ac11d1f68472882f27ecd63044a81031893
luiz fernando <108934829+Luizfernandouu@users.noreply.github.com>
2022-10-19 12:52:50 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,35 +1,12 @@
-let tabuada = 111;
-function escreva(){
-    document.write("Tabuada do " + tabuada + "<br>");
-    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
-}
+function media(){
+    let n1 = document.getElementById("n1").value
+    let n2 = document.getElementById("n2").value
+    let n3 = document.getElementById("n3").value
+    let n4 = document.getElementById("n4").value
 
-let lista = ["Jesus","Spessato","Duarte","Jorge","Neuza","Gilmar","Jossyara"];
+    let r = (Number(n1)+Number(n2)+Number(n3)+Number(n4)+Number(n5)+Number(n6))/4;
+    document.getElementById("result").innerHTML = "media: "+ r;
 
-function mostraLista(){
-    document.write("Tamanho da lista: " + lista.length + "<br>");
-    for(let i = 0; i < lista.length; i++){
-        document.write("Professor: " + lista[i] + "<br>");
-    }
-}
-
-function multiplica(){
-    for(let i = 6; i <= 8; i++){
-        document.write("Tabuada do " + i + "<br>");
-        for(let j = 1; j <= 10; j++){
-            document.write(i + " x "+j+" = " + (i*j) + "<br>");
-        }
-        document.write("<br>");
-    }
 }
 
 
